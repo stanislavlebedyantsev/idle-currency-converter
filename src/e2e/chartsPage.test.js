48af738e2b9b9d2fb9ec343b47e4a3dcd4dba461
stanislavlebedyantsev <stanislav.lebedyantsev@gmail.com>
2021-03-26 11:45:49 +0300

    Completed the creation of e2e tests

--- src/e2e/chartsPage.test.js
+++ src/e2e/chartsPage.test.js
@@ -5,22 +5,28 @@ expect.extend({ toMatchImageSnapshot });
 const timeout = process.env.SLOWMO ? 30000 : 10000;
 
 describe('visual reg. test', () => {
-  let browser, page;
+  let browser, page, image;
   beforeAll(async () => {
     browser = await puppeteer.launch({ headless: false });
 
     page = await browser.newPage();
     await page.goto('http://localhost:3000');
+    await page.click('[data-testid=email]');
+    await page.keyboard.type('sstom.r@gmail.com');
+    await page.click('[data-testid=password]');
+    await page.keyboard.type('123123');
+    await page.click('[data-testid=signIn]');
+    await page.waitForNavigation();
   }, timeout);
 
   afterAll(async () => {
     await browser.close();
   });
-  test(
-    'testing homepage',
+	test(
+    'testing chart page',
     async () => {
-      const image = await page.screenshot();
-
+      await page.click('[data-testid=chartsBtn]');
+      image = await page.screenshot();
       expect(image).toMatchImageSnapshot();
     },
     timeout
