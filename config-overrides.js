cf742a7e27201aed6b6dfe559c064260594337c2
stanislavlebedyantsev <stanislav.lebedyantsev@gmail.com>
2021-04-02 10:17:52 +0300

    Added chunkhash to chunk name

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,14 @@
 const { alias, configPaths } = require('react-app-rewire-alias');
 
 module.exports = function override(config) {
-  config.optimization.splitChunks = { chunks: 'all', maxSize: 250000, minSize: 150000};
+  config.optimization.splitChunks = {
+    chunks: 'all',
+    maxSize: 250000,
+    minSize: 150000,
+  };
+  config.output = {
+    ...config.output,
+    chunkFilename: 'chunk-[name].[chunkhash].js',
+  };
   return config && alias(configPaths('./tsconfig.paths.json'))(config);
 };
